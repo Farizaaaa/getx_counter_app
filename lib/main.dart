63e6f1c720a1641a2a117ea7fb15ccd72b7d0202
Farizaaaa <farizaaaa10@gmail.com>
2023-11-11 08:14:38 +0530

    adding readme

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(title: "GetX CounterApp",),
+      home: MyHomePage(title: "GetX CounterApp Demo",),
 
       /// initialBinding: TextFieldBinding(),
       debugShowCheckedModeBanner: false,
